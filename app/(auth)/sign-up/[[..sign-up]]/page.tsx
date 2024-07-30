b9a17b0d3c9c18ca6f6577acd78e9cfb4235dc16
Vinit-Nagar <vinitnagar21@gmail.com>
2024-07-30 18:25:37 +0530

    authentication

---
+++ app/(auth)/sign-up/[[..sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const SignUnPage = () => {
+    return (
+        <div className='auth-page'>
+            <SignUp />
+        </div>
+    )
+}
+
+export default SignUnPage
--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Document = () => {
@@ -9,6 +10,12 @@ const Document = () => {
                 <div className='flex w-fit gap-2 items-center justify-center'>
                     <p className='document-title'>This is the test document</p>
                 </div>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </Header>
             <Editor />
         </div>
