736bfd734f6897229837096c1ed91738cda71d41
Vinit-Nagar <vinitnagar21@gmail.com>
2024-07-31 20:26:53 +0530

    Real-Time Features - Liveblocks Authenticate User with Real-time Features

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,23 +1,10 @@
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+
 import React from 'react'
 
 const Document = () => {
     return (
         <div>
-            <Header>
-                <div className='flex w-fit gap-2 items-center justify-center'>
-                    <p className='document-title'>This is the test document</p>
-                </div>
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-            </Header>
-            <Editor />
+            
         </div>
     )
 }
