3856243f593d61628d6520125154c5f709cb4d51
Vinit-Nagar <vinitnagar21@gmail.com>
2024-08-01 02:44:01 +0530

    home page

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,11 +1,12 @@
 
+import CollaborativeRoom from '@/components/CollaborativeRoom'
 import React from 'react'
 
 const Document = () => {
     return (
-        <div>
-            
-        </div>
+        <main className='flex flex-col items-center w-full'>
+            <CollaborativeRoom />
+        </main>
     )
 }
 
